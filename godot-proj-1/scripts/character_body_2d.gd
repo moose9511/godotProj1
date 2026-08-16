83e4eb4db0658101b69599ae58c3c709cfd512fb
Moose <kalenjonathan@hotmail.com>
2026-08-15 23:28:41 -0500

    gkhy

--- godot-proj-1/scripts/character_body_2d.gd
+++ godot-proj-1/scripts/character_body_2d.gd
@@ -48,12 +48,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	# do wall slide and jump
-	if direction != 0 and velocity.x == 0 and !is_on_floor():
+	if direction != 0 and velocity.x == 0 and !is_on_floor() and can_move:
 
 		if Input.is_action_pressed("jump"):
+			can_shoot = true
 			disable_movement()
 			direction = -direction
-			extra_velocity.x = direction * speed
+			velocity.x = direction * speed
+			extra_velocity.x =  -extra_velocity.x / 2
 			velocity.y = -jump_speed / 2
 		else:
 			velocity.y = 0
