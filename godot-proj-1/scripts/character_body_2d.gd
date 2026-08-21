b52955afc089499dc74c423eadd9a8616b523995
Moose <kalenjonathan@hotmail.com>
2026-08-21 13:54:51 -0500

    xcvb

--- godot-proj-1/scripts/character_body_2d.gd
+++ godot-proj-1/scripts/character_body_2d.gd
@@ -1,46 +1,66 @@
 extends CharacterBody2D
 
-@onready var timer: Timer = $Timer
+@onready var ledge_jump_timer: Timer = $Timers/LedgeJump
+@onready var disable_movement_timer: Timer = $Timers/DisableMovement
 @onready var ray_left: RayCast2D = $RayLeft
 @onready var ray_right: RayCast2D = $RayRight
+@onready var ammo: AnimatedSprite2D = $ammo
+@onready var reload: AnimatedSprite2D = $reload
+@onready var gun: Sprite2D = $gun
+@onready var player_sprite: AnimatedSprite2D = $playerSprite
 
 @export var jump_speed: float
 @export var speed: float
 @export var shoot_force: float
-@export var shoot_friction_ground: float
-@export var shoot_friction_air: float
+@export var ground_friction: float
+@export var air_friction: float
+@export var slide_friction: float
 @export var is_grounded: bool
+@export var reload_time: float
 
 var extra_velocity: Vector2
 var direction: float
 var can_shoot: bool = true
 var can_move: bool = true
+var jumped: bool = true
+
+func _ready() -> void:
+	reload.sprite_frames.set_animation_speed("reload", reload.sprite_frames.get_frame_count("reload") / reload_time)
 
 func _physics_process(delta: float) -> void:
 	
-	if velocity != Vector2.ZERO:
-		print(" " + str(velocity))
+	# Get the input direction and handle the movement/deceleration.
+	if can_move:
+		direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
 	
-	# Add the gravity.
+	var pressed_jump := Input.is_action_pressed("jump")
+	var dir := direction * speed + extra_velocity.x
+	
+	# midair and grounded logic
 	if not is_on_floor():
+		
+		# gives a little extra time to jump when falling off edge
+		if !jumped:
+			if ledge_jump_timer.time_left == 0:
+				ledge_jump_timer.start()
+				print("asdf")
+			if pressed_jump and dir != 0:
+				jump( )
+				
 		velocity += get_gravity() * delta
 	else:
-		can_shoot = true
+		
+		enable_shooting(true)
 		velocity.y = 0
 		extra_velocity.y = 0
+		jumped = false
 		
-
-	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
-		extra_velocity.y += -jump_speed
-
-	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_axis("move_left", "move_right")
-	if can_move:
-		velocity.x = direction * speed
-	
+		if pressed_jump:
+			jump()
+			
 	if Input.is_action_just_pressed("shoot") and can_shoot:
-		can_shoot = false
+		enable_shooting(false)
 		var force:Vector2 = -(get_global_mouse_position() - position).normalized() * shoot_force
 		extra_velocity = force
 		velocity.y = 0
@@ -53,35 +73,77 @@ func _physics_process(delta: float) -> void:
 	velocity += extra_velocity
 	move_and_slide()
 	
-	# do wall slide and jump
-	var dir := velocity + extra_velocity
-	if ((ray_left.is_colliding() and sign(dir.x) == -1) or (ray_right.is_colliding() and sign(dir.x) == 1)) and (direction != 0 or extra_velocity.x != 0):
-		print('a')
+	# do automatic wall jump
+	if (dir != 0 and ((ray_left.is_colliding() and sign(dir) == -1) or (ray_right.is_colliding()
+		 and sign(dir) == 1)) and (direction != 0 or extra_velocity.x != 0)):
+			
 		# bounces off wall
-		if Input.is_action_pressed("jump"):
-			can_shoot = true
+		if !Input.is_action_pressed("slide"):
+			enable_shooting(true)
 			disable_movement()
-			direction = -direction
-			#velocity.x = direction * speed
-			extra_velocity.x = -extra_velocity.x 
-			velocity.y = -jump_speed / 2
+			direction = -sign(dir)
+			velocity.x = direction * speed
+			extra_velocity.x = -sign(dir) * speed / 2
+			velocity.y = -jump_speed / 1.5
 		# slides down wall
 		else:
-			velocity.y = 0
+			#velocity.y = 0
+			extra_velocity.x = 0
 	
 	# undo extra velocity to avoid accumulation
 	velocity -= extra_velocity
 	
 	# apply friction to extra force
-	if is_on_floor():
-		extra_velocity = extra_velocity.move_toward(Vector2.ZERO, delta * shoot_friction_ground)
+	if is_on_floor() and Input.is_action_pressed("slide"):
+		extra_velocity = extra_velocity.move_toward(Vector2.ZERO, delta * slide_friction)
+	elif is_on_floor():
+		extra_velocity = extra_velocity.move_toward(Vector2.ZERO, delta * ground_friction)
 	else:
-		extra_velocity = extra_velocity.move_toward(Vector2.ZERO, delta * shoot_friction_air)
+		extra_velocity = extra_velocity.move_toward(Vector2.ZERO, delta * air_friction)
+		
+	# apply flip
+	process_flip()
 		
 func disable_movement():
 	can_move = false
-	timer.start()
+	disable_movement_timer.start()
 
+# sets whether or not the player can shoot
+# sets up animation stuff
+func enable_shooting(state: bool):
+	if !can_shoot and state and !reload.is_playing():
+		reload.visible = true
+		reload.play() 
+	
+	if !state:
+		ammo.set_frame_and_progress(1, 0)
+		can_shoot = false
+	
+func process_flip():
+	var mouse_position = get_global_mouse_position() - position + Vector2(0, 10)
+	gun.rotation = atan2(mouse_position.y, mouse_position.x) 
+	
+	if gun.rotation < deg_to_rad(-90) or gun.rotation > deg_to_rad(90):
+		gun.flip_v = true
+		player_sprite.flip_h = true
+	else:
+		gun.flip_v = false
+		player_sprite.flip_h = false
+		
+func jump():
+	extra_velocity.x = move_toward(extra_velocity.x, 0, speed)
+	extra_velocity.y += -jump_speed
+	jumped = true
 
 func _on_timer_timeout() -> void:
 	can_move = true
+
+
+func _on_reload_animation_finished() -> void:
+	ammo.set_frame_and_progress(0, 0)
+	reload.visible = false
+	can_shoot = true
+
+
+func _on_ledge_jump_timeout() -> void:
+	jumped = true
